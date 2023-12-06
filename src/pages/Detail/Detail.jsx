e9aa86f655f672e9b0293d622add2be5f983b43f
JooyoungO <ohjoo922@naver.com>
2023-12-06 20:55:49 +0900

    12/06 변경사항

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,62 +1,79 @@
-import cat from "../../images/cat.png";
-import "./Detail.css";
-import { useNavigate } from "react-router-dom";
+// import React, { useState, useEffect } from 'react';
+//import { useParams } from 'react-router-dom';
+// import axios from 'axios'; //*api 불러오면 주석해제
+import React, { useState } from 'react'; //api 불러오면 삭제
+import './Detail.css';
 
 function Detail() {
-  const navigate = useNavigate();
+    // 임시 데이터로 상태 초기화
+    const [model] = useState({
+        image: 'https://via.placeholder.com/150',
+        title: '임시 제목',
+        userName: '사용자 이름',
+        participantCount: '100',
+        reward: '50',
+        conditions: ['고양이 전신 사진이 촬영되어야 합니다.'],
+        description: '😎 안녕하세요. 저희는 고양이 전신 사진을 100장 정도 확보하는 것을 목표로 하고 있습니다...',
+    });
 
-  const onClickBtn = () => {
-    navigate(-1);
-  };
+// function Detail() {
+//     const [model, setModel] = useState(null);
+//     const { id } = useParams();
+
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             const token = "test";
+//             try {
+//                 const res = await axios.get(`http://172.16.228.187:8080/collects/${id}`, {  // id를 사용하는 URL
+//                     headers: {
+//                         "Content-Type": `application/json;charset=UTF-8`,
+//                         "Accept": "application/json",
+//                         "Authorization": "Bearer " + token,
+//                     },
+//                     responseType: "json"
+//                 });
+    
+//                 setModel(res.data);  // API 응답으로 상태 업데이트
+//             } catch (error) {
+//                 console.error('Error fetching data:', error);
+//             }
+//         }
+    
+//         fetchData();
+//     }, [id]);   //*api 불러오면 주석해제
 
     return (
-      <div className="Detail-wrapper">
-        <button onClick={onClickBtn}>뒤로가기</button> {/** 뒤로가기 버튼 추가 */}
-        <div className="Detail-image">
-          <img src={cat} alt=""></img>
-        </div>
-        <div className="Detail-title">
-          <p>
-            <b>집 고양이 전신 사진 데이터 확보</b>
-          </p>
-        </div>
-        <div className="Detail-user">
-          <p style={{ color: "#6F6F6F", textAlign: "start" }}>고양이 사랑</p>
-          <p style={{ color: "#6F6F6F", textAlign: "left" }}>👥 100명</p>
-          <p style={{ color: "#214FC7", textAlign: "right" }}>
-            <b>💵 50 DBC 지급</b>
-          </p>
-        </div>
-        <div className="Detail-condition">
-          <h4
-            style={{ textAlign: "left", marginTop: "16px", marginLeft: "16px" }}
-          >
-            [사진조건]
-          </h4>
-          <ul>
-            <li style={{ textAlign: "left" }}>
-              고양이 전신 사진이 촬영되어야 합니다.
-            </li>
-            <li style={{ textAlign: "left" }}>
-              고양이 전신 사진이 촬영되어야 합니다.
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3 style={{ textAlign: "left", marginLeft: "16px" }}>소개</h3>
-          <p style={{ textAlign: "left", marginLeft: "16px" }}>
-            😎 안녕하세요. 저희는 고양이 전신 사진을 100장 정도 확보하는 것을
-            목표로 하고 있습니다. ‘고양이사랑'팀은 2023년부터 법인등록하여
-            현재는 고양이들의 복지 정책을 위해 힘쓰고 있습니다. 그러던 중 집
-            고양이의 전신 사진 데이터를 모아야만하는 상황이 있어 이렇게 사용하게
-            되었습니다. 어려운 부분은 없으니 시간 되신다면 참여해주시면 좋을 것
-            같습니다. 감사합니다.
-          </p>
-        </div>
-        <div className="footer-button">참여하기</div>
-      </div>
-    );
-  };
+        <div className='Detail-wrapper'>
+            <div className='Detail-image'>
+                <img src={model ? model.image : 'default_image.png'} alt={model ? model.title : 'Loading...'} />
+            </div>
+            <div className='Detail-title'>
+                <p>{model ? model.title : "Loading..."}</p>
+            </div>
+            <div className='Detail-user'>
+            <p style={ { color : '#6F6F6F', textAlign : 'start' }} >{model ? model.userName : "Loading..."}</p>
+            <p style={ { color : '#6F6F6F', textAlign : 'left' }}>👥 {model ? model.participantCount : "Loading..."}명</p>
+            <p style={ { color : '#214FC7', textAlign : 'right' }}><b>💵 {model ? model.reward : "Loading..."} DBC 지급</b></p>
+            </div>
+            <div className='Detail-condition'>
+                <h4 style={{ textAlign:'left', marginTop: '16px', marginLeft: '16px'}}>[사진조건]</h4>
+                <ul>
+                    {/* 조건 내용을 동적으로 렌더링 */}
+                    {model && model.conditions.map((condition, index) => (
+                        <li key={index} style={{ textAlign: 'left'}}>{condition}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h3 style={{ textAlign: 'left', marginLeft: '16px'}}>소개</h3>
+                <p style={{textAlign: 'left', marginLeft: '16px', marginRight:'16px'}}>
+                    {model ? model.description : 'Loading...'}
+                </p>
+            </div>
 
+            <div className='footer-button'>참여하기</div>
+        </div>
+    );
+}
 
 export default Detail;
--- src/pages/Reward/Reward.jsx
+++ src/pages/Reward/Reward.jsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Trading from '../../components/Trading/Trading';
 import './Reward.css';
 import cat from "../../images/cat.png";
 
 
 function Reward() {
+
+    const [model] = useState({
+        myReward: '본인이 가진 리워드',
+        image: 'https://via.placeholder.com/150',
+        name: '거래한 사람이름',
+        date: '거리한 날짜',
+        trading: '입출금 내역',
+        balance: '잔액'
+    });
+
     return (
         <div className='reward'>
             <p style={{ 
                 color: 'black', margin:'60px 0px 0px 16px', fontSize:'16px', fontSize:'16px'}}>당신의 리워드는</p>
-            <p style={{margin:'6px 16px', fontSize:'16px', fontSize:'32px'}}><b>1,012,716 DBC </b></p>
+            <p style={{margin:'6px 16px', fontSize:'16px', fontSize:'32px'}}><b>{model ? model.myReward : "Loading..."} </b></p>
             <p style={{margin:'6px 16px', fontSize:'16px'}}>입니다.</p>
             {/** 버튼 컴포넌트 들어올 자리, 지금은 만든게 없어서 대체 */}
             <div className='reward-button'><b>이체하기</b></div>
             <p style={{color:'#A5A5A5', marginLeft:'16px'}}>거래내역</p>
             <Trading
-            image={cat} 
-            name='지갑 출금'
-            date='2023. 12. 05 16:25'
-            trading='-8,000 DBC'
-            balance='1012,716 DBC'
+            image src={model ? model.image : "defaul_image.png"} 
+            name= {model ? model.name : "Loading..."}
+            date= {model ? model.date : "Loading..."}
+            trading= {model ? model.trading : "Loading..."}
+            balance= {model ? model.balance : "Loading..."}
             />
             <Trading
-            image={cat} 
-            name='지갑 출금'
-            date='2023. 12. 05 16:25'
-            trading='-8,000 DBC'
-            balance='1012,716 DBC'
+            image src={model ? model.image : "defaul_image.png"} 
+            name= {model ? model.name : "Loading..."}
+            date= {model ? model.date : "Loading..."}
+            trading= {model ? model.trading : "Loading..."}
+            balance= {model ? model.balance : "Loading..."}
             />
             <Trading
-            image={cat} 
-            name='지갑 출금'
-            date='2023. 12. 05 16:25'
-            trading='-8,000 DBC'
-            balance='1012,716 DBC'
+            image src={model ? model.image : "defaul_image.png"} 
+            name= {model ? model.name : "Loading..."}
+            date= {model ? model.date : "Loading..."}
+            trading= {model ? model.trading : "Loading..."}
+            balance= {model ? model.balance : "Loading..."}
             />
             <Trading
-            image={cat} 
-            name='지갑 출금'
-            date='2023. 12. 05 16:25'
-            trading='-8,000 DBC'
-            balance='1012,716 DBC'
+            image src={model ? model.image : "defaul_image.png"} 
+            name= {model ? model.name : "Loading..."}
+            date= {model ? model.date : "Loading..."}
+            trading= {model ? model.trading : "Loading..."}
+            balance= {model ? model.balance : "Loading..."}
             />
         </div>
     )
