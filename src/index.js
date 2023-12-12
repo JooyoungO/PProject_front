6867e5947cd2e3721c0b89e70285522b187bf75a
JooyoungO <ohjoo922@naver.com>
2023-12-12 12:29:06 +0900

    12/12 User2 page

--- src/index.js
+++ src/index.js
@@ -39,34 +39,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './pages/CardList/CardList';
 // import App from './App'; // App 컴포넌트 임포트 (배포할 때 주석 풀기)
-//import Delete from './pages/Delete/Delete';
-import { BrowserRouter } from 'react-router-dom'
-import DataSubmission from './pages/DataSubmission/DataSubmission';
-import DataSubmission2 from './pages/DataSubmission2/DataSubmission2';
-import SignupType from './pages/SignupType/SignupType';
-import SignupType2 from './pages/SignupType2/SignupType2';
-import CardListCollector from './pages/CardListCollector/CardListCollector';
+import DataSubmissionForm from './pages/DataSubmissionForm/DataSubmissionForm';
+// import RewardSend from './pages/RewardSend/RewardSend';
 import CardListTag from './pages/CardListTag/CardListTag';
-import Detail from './pages/Detail/Detail';
-import CardListCollector2 from './pages/CardListCollector2/CardListCollector2';
-import Reward from './pages/Reward/Reward';
-import DepositSuccess from './pages/DepositSuccess/DepositSuccess';
-import SendSuccess from './pages/SendSuccess/SendSuccess';
-import DetailCollector from './pages/DetailCollector/DetailCollector';
-import Reward2 from './pages/Reward2/Reward2';
-import RewardDeposit from './pages/RewardDeposit/RewardDeposit';
-import RewardSend from './pages/RewardSend/RewardSend';
-import User from './pages/User/User';
-import BackDataTradeList from './components/BackDataTradeList/BackDataTradeList';
+import CardList from './pages/CardList/CardList';
 import Delete from './pages/Delete/Delete';
-import AccountDeletion from './pages/AccountDeletion/AccountDeletion';
+import DetailCollector from './pages/DetailCollector/DetailCollector';
+import CardListCollector2 from './pages/CardListCollector2/CardListCollector2';
+import RewardDeposit from './pages/RewardDeposit/RewardDeposit';
+import Reward2 from './pages/Reward2/Reward2';
+
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <RewardDeposit /> 
+    <Reward2 /> 
   </BrowserRouter>
-);
-
+); 
