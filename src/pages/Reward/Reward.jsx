129fe89bc61e7277769167ae6347d136ca815791
JooyoungO <ohjoo922@naver.com>
2023-12-14 20:21:56 +0900

    12/14 수집자 제공자 Reward 이체, 입금 페이지

--- src/pages/Reward/Reward.jsx
+++ src/pages/Reward/Reward.jsx
@@ -1,81 +1,77 @@
-import React, { useState } from 'react';
-import Trading from '../../components/Trading/Trading';
-import { useNavigate } from 'react-router-dom';
-import './Reward.css';
-import DataTradeList from '../../components/DataTradeList/DataTradeList';
-import RewardNavbar from '../../components/RewardNavbar/RewardNavbar';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Trading from "../../components/Trading/Trading";
+import { useNavigate } from "react-router-dom";
+import "./Reward.css";
+import DataTradeList from "../../components/DataTradeList/DataTradeList";
+import RewardNavbar from "../../components/RewardNavbar/RewardNavbar";
 
 function Reward() {
-    const navigate = useNavigate();
-    const [model] = useState({
-        myReward: '본인이 가진 리워드',
-        image: 'https://via.placeholder.com/150',
-        name: '거래한 사람이름',
-        date: '거리한 날짜',
-        trading: '입출금 내역',
-        balance: '잔액'
-    });
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [model, setModel] = useState(null);
 
-    const handleTransferClick = () => {
-        navigate('/RewardSend'); // RewardSend 컴포넌트로 이동
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = "";
+      try {
+        const response = await axios.get("/reward/history", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
+        if (response.status === 200) {
+          setModel(response.data);
+        }
+      } catch (error) {
+        console.error("API 호출 중 오류 발생:", error);
+      }
     };
 
-    return (
-        <>
-        <DataTradeList listTitle={"리워드"} />
-        <div className='reward'>
-            <p style={{ 
-                color: 'black', margin:'30px 0px 0px 16px', fontSize:'16px'}}>당신의 리워드는</p>
-            <p style={{margin:'6px 16px', fontSize:'32px'}}><b>{model ? model.myReward : "Loading..."} </b></p>
-            <p style={{margin:'6px 16px', fontSize:'16px'}}>입니다.</p>
-            <div className='reward-button' onClick={handleTransferClick}><b>이체하기</b></div>
-            <p style={{color:'#A5A5A5', marginLeft:'16px'}}>거래내역</p>
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
+    fetchData();
+  }, [id]);
+
+  const handleTransferClick = () => {
+    navigate("/RewardSend");
+  };
+
+  return (
+    <>
+      <DataTradeList listTitle={"리워드"} />
+      <div className="reward">
+        <p
+          style={{
+            color: "black",
+            margin: "30px 0px 0px 16px",
+            fontSize: "16px",
+          }}
+        >
+          당신의 리워드는
+        </p>
+        <p style={{ margin: "6px 16px", fontSize: "32px" }}>
+          <b>{model ? model.totalReward : "Loading..."} </b>
+        </p>
+        <p style={{ margin: "6px 16px", fontSize: "16px" }}>입니다.</p>
+        <div className="reward-button" onClick={handleTransferClick}>
+          <b>이체하기</b>
         </div>
-        <RewardNavbar />
-        </>
-    )
+        <p style={{ color: "#A5A5A5", marginLeft: "16px" }}>거래내역</p>
+        {model && model.data &&
+          model.data.map((data) => (
+            <Trading
+              src={data.img}
+              name={data.sender}
+              date={data.createdAt}
+              trading={data.amount}
+              balance={data.total}
+            />
+          ))}
+      </div>
+      <RewardNavbar />
+    </>
+  );
 }
 
 export default Reward;
--- src/pages/Reward2/Reward2.jsx
+++ src/pages/Reward2/Reward2.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import Trading from '../../components/Trading/Trading';
 import './Reward2.css';
 import DataTradeList from '../../components/DataTradeList/DataTradeList';
@@ -9,15 +11,38 @@ import { useNavigate } from 'react-router-dom';
 function Reward2({listTitle}) {
 
     const navigate = useNavigate();
+    const { id } = useParams();
+    const [model, setModel] = useState(null);
 
-    const [model] = useState({
-        myReward: '본인이 가진 리워드',
-        image: 'https://via.placeholder.com/150',
-        name: '거래한 사람이름',
-        date: '거리한 날짜',
-        trading: '입출금 내역',
-        balance: '잔액'
-    });
+    useEffect(() => {
+        const fetchData = async () => {
+          const token = "";
+          try {
+            const response = await axios.get("/reward/history", {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+              },
+            });
+            if (response.status === 200) {
+              setModel(response.data);
+            }
+          } catch (error) {
+            console.error("API 호출 중 오류 발생:", error);
+          }
+        };
+    
+        fetchData();
+      }, [id]);
+
+    // const [model] = useState({
+    //     myReward: '본인이 가진 리워드',
+    //     image: 'https://via.placeholder.com/150',
+    //     name: '거래한 사람이름',
+    //     date: '거리한 날짜',
+    //     trading: '입출금 내역',
+    //     balance: '잔액'
+    // });
 
     function onSendClickHandler() {
         navigate('/RewardDeposit');
@@ -33,34 +58,16 @@ function Reward2({listTitle}) {
             <p style={{margin:'6px 16px', fontSize:'16px'}}>입니다.</p>
             <div className='DepositButton' onClick={onSendClickHandler}>입금하기</div>
             <p style={{color:'#A5A5A5', marginLeft:'16px'}}>거래내역</p>
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
+            {model && model.data &&
+            model.data.map((data) => (
+                <Trading 
+                    src={data.img}
+                     name={data.sender}
+                     date={data.createdAt}
+                     trading={data.amount}
+                     balance={data.total}
+                />
+            ))}
         </div>
         <PointNavbar />
     </>
